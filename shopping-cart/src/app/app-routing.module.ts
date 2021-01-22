c21ca2eda71e09db8ebc7408a74e88d6559f96f1
izak270 <58972658+izak270@users.noreply.github.com>
2021-01-22 15:59:20 +0200

    add routes, cart and shop commponents

--- shopping-cart/src/app/app-routing.module.ts
+++ shopping-cart/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'shop',
+    component: ShopComponent,
+},
+  {
+    path: 'cart',
+    component: CartComponent,
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import bootstrap from "bootstrap";
+import { CartComponent } from './components/cart/cart.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShopComponent } from './components/shop/shop.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    HeaderComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
