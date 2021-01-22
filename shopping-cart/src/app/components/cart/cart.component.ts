b9061ed4d380d289dcfdfc89c8ae46b40846cca9
izak270 <58972658+izak270@users.noreply.github.com>
2021-01-22 16:42:36 +0200

    add fetch for items to the cart and some price of all items

--- shopping-cart/src/app/components/cart/cart.component.ts
+++ shopping-cart/src/app/components/cart/cart.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  products = [];
+  somePrice = 0;
+
   constructor() { }
 
   ngOnInit() {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(
+        data => {
+          console.log(data);
+          console.log(data[0].image);
+
+          this.products = data
+          this.products.forEach(item => {
+            item.price
+            this.somePrice += item.price
+          });
+        }
+      )
   }
 
 }
