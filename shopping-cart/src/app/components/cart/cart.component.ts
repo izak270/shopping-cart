2aecf7bef6ea3b17748bf9809aa20c6852c5a5c7
izak270 <58972658+izak270@users.noreply.github.com>
2021-01-22 17:21:41 +0200

    add items to local storage and add buttons for add and remove items

--- shopping-cart/src/app/components/cart/cart.component.ts
+++ shopping-cart/src/app/components/cart/cart.component.ts
@@ -9,24 +9,43 @@ export class CartComponent implements OnInit {
 
   products = [];
   somePrice = 0;
+  shippingPrice: number
 
   constructor() { }
 
   ngOnInit() {
-    fetch('https://fakestoreapi.com/products')
-      .then(res => res.json())
-      .then(
-        data => {
-          console.log(data);
-          console.log(data[0].image);
+    this.products = JSON.parse(localStorage.getItem('productList'));
+    console.log(this.products);
+    if (!this.products[0]) {
+      fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(
+          data => {
+            this.products = data
+            this.products.forEach(item => {
+              item.price
+              this.somePrice += item.price
+              this.shippingPrice = this.products.length > 5 ? this.calcShippingPrice() : 2
+              item.quantity = 1
+            });
+            localStorage.setItem('productList', JSON.stringify(this.products));
+            this.somePrice = Math.round(this.somePrice)
+          }
+        )
+    }
+  }
+  calcShippingPrice() {
+    return this.products.length * 3
+  }
 
-          this.products = data
-          this.products.forEach(item => {
-            item.price
-            this.somePrice += item.price
-          });
-        }
-      )
+  addItem(index) {
+    this.products[index].quantity++
+    localStorage.setItem('productList', JSON.stringify(this.products));
+  }
+
+  remove(index) {
+    this.products.splice(index, 1);
+    localStorage.setItem('productList', JSON.stringify(this.products));
   }
 
 }
